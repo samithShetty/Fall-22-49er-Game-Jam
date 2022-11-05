0be356059f64be0c649ecdca68b070a0e1b33fe1
Samith Shetty <samithshetty0@gmail.com>
2022-11-05 16:32:37 -0400

    Added water collision

--- Snowman.gd
+++ Snowman.gd
@@ -1,15 +1,9 @@
 extends RigidBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 export var speed = 500
 export var acceleration = 3.0
 var lookDir = Vector2(0,1)
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
 
 func _physics_process(delta):
 	linear_velocity = Vector2(clamp(linear_velocity.x,-speed,speed),clamp(linear_velocity.y,-speed,speed))
@@ -17,19 +11,14 @@ func _physics_process(delta):
 	var y = Input.get_action_strength("up") - Input.get_action_strength("down")
 	apply_impulse(Vector2(0,0), Vector2(acceleration*x,acceleration*-y))
 	
-	lookDir = lerp(lookDir, Vector2(x,-y), .07)
-	look_at(lookDir + position)
+	if x != 0 or y != 0:
+		lookDir = lerp(lookDir, Vector2(x,-y), .07)
+		look_at(lookDir + position)
 	
 	var space_state = get_world_2d().direct_space_state
 	# use global coordinates, not local to node
-	var isInWater = space_state.intersect_ray(Vector2(0,0), get_linear_velocity().rotated(-get_global_rotation()).normalized()*35, [], 7, true, true)
-	if isInWater:
-		print("You fell in water!")
-	update()
+	var waterCollision = space_state.intersect_ray(position, position + get_linear_velocity().rotated(-get_global_rotation()).normalized()*30, [self], 7, true, true)
 
-func _draw():
-	draw_line(get_linear_velocity().rotated(-get_global_rotation()).normalized()*30, get_linear_velocity().rotated(-get_global_rotation()).normalized()*35,Color(0,0,0),3,false)
-	
-func _process(delta):
-	if Input.is_key_pressed(KEY_SPACE):
-		print()
+
+func _on_WaterArea_area_entered(area):
+	print("DEATH")
--- WaterTrail.gd
+++ WaterTrail.gd
@@ -2,16 +2,35 @@ extends Line2D
 
 export var length = 500
 export var delay = 50
-var point_queue = []
+onready var player = get_parent().get_node("Snowman")
+onready var area = $WaterArea
 var point = Vector2()
+var prev_point = Vector2()
+var segments = []
 
+func _ready():
+	global_position = Vector2(0,0)
+	global_rotation = 0
+	prev_point = player.global_position
+	add_point(prev_point)
+	
+	
 func _process(delta):
 	global_position = Vector2(0,0)
 	global_rotation = 0
-	point = get_parent().global_position
-	point_queue.append(point)
+	point = player.global_position
+	add_point(point)
 	
-	add_point(point_queue.pop_front())
+	#Add segment collider
+	var new_segment = CollisionShape2D.new()
+	new_segment.shape = SegmentShape2D.new()
+	new_segment.shape.a = prev_point
+	new_segment.shape.b = point
+	area.add_child(new_segment)
+	segments.append(new_segment)
 	
+	prev_point = point
+
 	while get_point_count() > length:
 		remove_point(0)
+		area.remove_child(segments.pop_front())
