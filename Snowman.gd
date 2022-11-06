0ac8341546762ef537bf3695c1a4621c4798b3be
Samith Shetty <samithshetty0@gmail.com>
2022-11-06 05:28:23 -0500

    Added drifty movement

--- Snowman.gd
+++ Snowman.gd
@@ -1,19 +1,29 @@
 extends RigidBody2D
 
+export var turn_speed = 0.05
 export var speed = 500
+export var max_speed = 500
 export var acceleration = 3.0
-var lookDir = Vector2(0,1)
+export var drift_speed =  0.05
+
+var lookDir = Vector2(1,0)
+var moveAngle = 0
 
 
 func _physics_process(delta):
-	linear_velocity = Vector2(clamp(linear_velocity.x,-speed,speed),clamp(linear_velocity.y,-speed,speed))
+	
 	var x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	var y = Input.get_action_strength("up") - Input.get_action_strength("down")
-	apply_impulse(Vector2(0,0), Vector2(acceleration*x,acceleration*-y))
 	
-	if x != 0 or y != 0:
-		lookDir = lerp(lookDir, Vector2(x,-y), .07)
-		look_at(lookDir + position)
+	if x: # Extra turn render rotation
+		moveAngle += sign(x) * turn_speed
+		lookDir = lookDir.slerp(Vector2(1,0).rotated(moveAngle + sign(x)*PI/2),0.1)
+	else: # Normal Smooth render rotation
+		lookDir = lookDir.slerp(Vector2(1,0).rotated(moveAngle),0.1)
+		
+	linear_velocity = Vector2(speed,0).rotated(moveAngle)
+	rotation = lookDir.angle()
+	
 	
 	var space_state = get_world_2d().direct_space_state
 	# use global coordinates, not local to node
