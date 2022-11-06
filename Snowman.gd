fe3ddd20e896a30db22779680970e022f0928b74
Samith Shetty <samithshetty0@gmail.com>
2022-11-06 18:16:50 -0500

    Fixed water collision and added jump

--- Snowman.gd
+++ Snowman.gd
@@ -2,12 +2,14 @@ extends RigidBody2D
 
 export var turn_speed = 0.05
 export var speed = 500
-export var max_speed = 500
-export var acceleration = 3.0
-var swingTime = 0
 
 var lookDir = Vector2(1,0)
 var moveAngle = 0
+var isJumping = false
+var jumpTime = 0
+var swingTime = 0
+onready var waterTrail = preload("res://WaterTrail.tscn")
+
 
 func _physics_process(delta):
 
@@ -15,15 +17,25 @@ func _physics_process(delta):
 		swingTime -= delta
 		if swingTime <= 0:
 			$SwordArea/CollisionShape2D.disabled = true
+	
+	if isJumping:
+		jumpTime -= delta
+		if jumpTime <= 0:
+			isJumping = false
+			$Sprites.scale.x = 1
+			$Sprites.scale.y = 1
+			var new_trail = waterTrail.instance()
+			get_parent().add_child(new_trail)
+
 
 	var x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	var y = Input.get_action_strength("up") - Input.get_action_strength("down")
 	
-	if x: # Extra turn render rotation
+	if x and not isJumping: # Extra turn render rotation
 		moveAngle += sign(x) * turn_speed
 		lookDir = lookDir.slerp(Vector2(1,0).rotated(moveAngle + sign(x)*PI/2),0.1)
 	else: # Normal Smooth render rotation
-		lookDir = lookDir.slerp(Vector2(1,0).rotated(moveAngle),0.1)
+		lookDir = lookDir.slerp(Vector2(1,0).rotated(moveAngle),0.04)
 		
 	linear_velocity = Vector2(speed,0).rotated(moveAngle)
 	rotation = lookDir.angle()
@@ -32,17 +44,17 @@ func _physics_process(delta):
 	# use global coordinates, not local to node
 	var waterCollision = space_state.intersect_ray(position, position + get_linear_velocity().rotated(-get_global_rotation()).normalized()*30, [self], 7, true, true)
 
-
 func _input(event):
 	if event.is_action_pressed("swing") and !$AnimationPlayer.is_playing():
 		$AnimationPlayer.play("swing")
 		$SwordArea/CollisionShape2D.disabled = false
 		swingTime = .4
-		
 
-func _on_WaterArea_area_entered(area):
-	print("DEATH")
-
+	if event.is_action_pressed("jump") and !isJumping:
+		isJumping = true
+		$Sprites.scale.x = 1.2
+		$Sprites.scale.y = 1.2
+		jumpTime = .3
 
 func _on_SwordArea_body_entered(body):
 	body.apply_impulse(Vector2(0,0),(body.position - $SwordArea.global_position).normalized() * 2000)
